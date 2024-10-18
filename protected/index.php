acf29d38a7688e89ca9d8adf6d42148aa1c983b7
MyKyo <yr09999mz@gmail.com>
2024-10-18 06:34:14 -0700

    Update index.php

--- protected/index.php
+++ protected/index.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Home</title>
   <style>
     body {
       font-family: Arial, sans-serif;
